3e1b0448012cdc17634cf6f27ef131a86b5ca4b5
malghailani <m6r3g@icloud.com>
2025-01-03 01:03:32 +0400

    Initial Commit

---
+++ sourcecontrol/sourcecontrolApp.swift
@@ -0,0 +1,17 @@
+//
+//  sourcecontrolApp.swift
+//  sourcecontrol
+//
+//  Created by Mohamed Al Ghailani on 03/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct sourcecontrolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ sourcecontrolTests/sourcecontrolTests.swift
@@ -0,0 +1,17 @@
+//
+//  sourcecontrolTests.swift
+//  sourcecontrolTests
+//
+//  Created by Mohamed Al Ghailani on 03/01/2025.
+//
+
+import Testing
+@testable import sourcecontrol
+
+struct sourcecontrolTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
